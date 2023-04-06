0351d1facd14ecf4f653aea5576c6b175caa1cee
AitoolsPro <kdf1688@gmail.com>
2023-04-06 20:34:22 +0800

    Your commit message

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -168,9 +168,16 @@ export const Chat: FC<Props> = memo(
             <div className="text-center text-gray-500 dark:text-gray-400">
             
             
-            <div className="mb-2" style={{ whiteSpace: "pre-wrap" }}>
-  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
-</div>
+            <div className="mb-2">
+      <div className="flex flex-row justify-between w-full sm:w-auto">
+        <span className="w-2/3 sm:w-auto truncate pr-2">
+          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
+        </span>
+        <button className="w-1/3 sm:w-auto whitespace-nowrap rounded-md bg-blue-600 hover:bg-blue-700 text-white font-medium px-3 py-1 transition duration-300 ease-in-out focus:outline-none">
+          Copy
+        </button>
+      </div>
+        </div>
 
              
               <div className="mb-2">
